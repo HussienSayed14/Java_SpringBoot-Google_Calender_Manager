21c0bacf58d367d010d11389ce8bba180e6b1b0f
HussienSayed14 <hussiens399@gmail.com>
2024-05-01 22:34:07 +0300

    Creating Event And Creator Model and specifying relation with fk index

--- src/main/java/com/ropulva/CalendarManagement/creator/CreatorModel.java
+++ src/main/java/com/ropulva/CalendarManagement/creator/CreatorModel.java
@@ -1,4 +1,28 @@
 package com.ropulva.CalendarManagement.creator;
 
+
+import com.ropulva.CalendarManagement.event.EventModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "creator", indexes = {
+        @Index(name = "idx_event_id", columnList = "event_id"),
+})
 public class CreatorModel {
+    @Id
+    @Column(name = "email")
+    private String email;
+    @Column(name = "name")
+    private String name;
+    @OneToOne
+    @JoinColumn(name = "event_id", unique = true)
+    private EventModel event;
+
+
 }
--- src/main/java/com/ropulva/CalendarManagement/event/EventModel.java
+++ src/main/java/com/ropulva/CalendarManagement/event/EventModel.java
@@ -1,4 +1,39 @@
 package com.ropulva.CalendarManagement.event;
 
+
+import com.ropulva.CalendarManagement.creator.CreatorModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "event")
 public class EventModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "summary")
+    private String summary;
+    @Column(name = "created")
+    private Timestamp created;
+    @Column(name = "updated")
+    private Timestamp updates;
+    @Column(name = "color_id")
+    private String colorId;
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private CreatorModel creator;
+
+
 }
