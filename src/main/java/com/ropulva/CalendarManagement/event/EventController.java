bab6fafc2814fbddec71c624ca5f9eba7a619991
HussienSayed14 <hussiens399@gmail.com>
2024-05-03 16:25:51 +0300

    Adding Create Event Object with validations

---
+++ src/main/java/com/ropulva/CalendarManagement/event/EventController.java
@@ -0,0 +1,13 @@
+package com.ropulva.CalendarManagement.event;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ropulva/ap1/v1/event")
+public class EventController {
+
+}
---
+++ src/main/java/com/ropulva/CalendarManagement/event/requests/CreateEventRequest.java
@@ -0,0 +1,37 @@
+package com.ropulva.CalendarManagement.event.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Getter
+@Setter
+public class CreateEventRequest {
+    private String description;
+
+    @NotEmpty
+    private String summary;
+    @NotEmpty
+    private String colorId;
+    @Email
+    @NotEmpty
+    private String creatorEmail;
+
+    @NotEmpty
+    private String creatorName;
+
+    @FutureOrPresent
+    private Date startDate;
+
+    @FutureOrPresent
+    private Date endDate;
+
+    private Time startTime;
+    private Time endTime;
+}
